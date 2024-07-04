f47329a2c471555246403bdefcbf65f447143b71
Levy <112917687+levyingx@users.noreply.github.com>
2024-07-04 03:00:56 -0300

    Update main.c

--- main.c
+++ main.c
@@ -1,24 +1,24 @@
 #include <stdio.h>
 
-// C(n, r) = C(n - 1, r) + C(n - 1, r - 1)
+#define MAX 15
 
-int matrix[10][10] = {0};
+int matrix[MAX][MAX] = {0};
 
-void generateTriangle() {
-  for (int i = 0; i < 10; i++) {
+void generate_triangle(int num) {
+  for (int i = 0; i <= MAX; i++) {
     matrix[i][0] = 1;
   }
 
-  for (int i = 1; i < 10; i++) {
-    for (int j = 1; j < 10; j++) {
+  for (int i = 1; i <= num; i++) {
+    for (int j = 1; j <= num; j++) {
       matrix[i][j] = matrix[i - 1][j] + matrix[i - 1][j - 1];
     }
   }
 }
 
-void printMatrix() {
-  for (int i = 0; i < 10; i++) {
-    for (int j = 0; j < 10; j++) {
+void print_matrix(int num) {
+  for (int i = 0; i <= num; i++) {
+    for (int j = 0; j <= num; j++) {
       if (matrix[i][j] > 0) {
         printf("%d ", matrix[i][j]);
       }
@@ -27,8 +27,24 @@ void printMatrix() {
   }
 }
 
+int prompt_user() {
+  int num = 0;
+  while (1) {
+    printf("How many lines? ");
+    scanf("%d", &num);
+    if (num < 1) {
+      printf("Number should be bigger or equal than 1.");
+    } else if (num > 15) {
+      printf("Limit of 15 lines exceeded.");
+    } else {
+      return num;
+    }
+  }
+}
+
 int main() {
-  generateTriangle();
-  printMatrix();
+  int x = prompt_user();
+  generate_triangle(x);
+  print_matrix(x);
   return 0;
 }
